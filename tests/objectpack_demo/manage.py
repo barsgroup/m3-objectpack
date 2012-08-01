f3c00f6f0d00d751d86a9c78c7ef18ad0d30d502
Пирогов Алексей <devnull@localhost>
2012-08-01 16:53:15 +0400

    Доработки демо-проекта

--- tests/objectpack_demo/manage.py
+++ tests/objectpack_demo/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import prepare_env
 from django.core.management import execute_manager
 import imp
 try:
---
+++ tests/objectpack_demo/version.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+File: version.py
+Description:
+'''
+
+__version__ = '2012.xxx'
+
+__appname__ = u'ObjectPack Demo'
+
+__require__ = {
+    # пакеты М3
+    'm3': '1.0',
+    'kladr': '1.1',
+    'm3_audit': '1.0',
+    'm3_users': '1.0',
+    'objectpack': 'default',
+}
+
+__require_local__ = {
+}
