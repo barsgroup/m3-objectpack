38ecf962e8245d7722fd06847dfb1324b4b47c85
Пирогов Алексей <devnull@localhost>
2012-08-02 15:51:47 +0400

    Локаль в демо

--- tests/objectpack_demo/settings.py
+++ tests/objectpack_demo/settings.py
@@ -29,11 +29,14 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Moscow'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
+
+# Date format
+DATE_FORMAT = '%d.%m.%Y'
 
 SITE_ID = 1
 
