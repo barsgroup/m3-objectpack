1733423e96f3a45ecfaf623f0c6b53a0e41a3c4e
Пирогов Алексей <devnull@localhost>
2012-08-07 12:55:36 +0400

    Демо-проект теперь имеет логируемый контроллер. Добавлен слушатель, модифицирующий отображение объектов в BandedColumnsPack

--- tests/objectpack_demo/demo_app/controller.py
+++ tests/objectpack_demo/demo_app/controller.py
@@ -9,8 +9,11 @@ from m3.ui import actions as m3_actions
 from objectpack import observer
 
 
+def logger(msg):
+    print "Observer: %s" % msg
+
 # Наблюдатель
-obs = observer.Observer()
+obs = observer.Observer(logger=logger, verbose_level=observer.Observer.LOG_MORE)
 
 
 action_controller = observer.ObservableController(obs, "/actions")
@@ -23,4 +26,15 @@ class Listener(object):
 
     def after(self, request, context, response):
         # подмена заголовка окна
-        response.data.title = u'Му-ха-ха!'
+        response.data.title = u'Му-ха-ха! (%s)' % self.action.__class__.__name__
+
+
+@obs.subscribe
+class StarToHash(object):
+
+    listen = ['fakemodel/.*']
+
+    def prepare_obj(self, obj):
+        if obj.get('field1', None) == u'*':
+            obj['field1'] = '#'
+        return obj
