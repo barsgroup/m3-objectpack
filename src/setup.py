1b5a8c694a0800cc5b5228fb6af0924aff6e84a6
Пирогов Алексей <devnull@localhost>
2013-07-23 16:21:35 +0400

    Фикс умолчательной декларации контекста

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="objectpack",
-    version="0.7.3.7",
+    version="0.7.3.8",
     description=read('DESCRIPTION'),
     license="GPL",
     keywords="django m3 m3-contrib",
