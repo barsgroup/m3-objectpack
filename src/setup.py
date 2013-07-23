90d1b283bf166ce8f971d9cae47ed3da2262e91a
Пирогов Алексей <devnull@localhost>
2013-07-23 16:47:51 +0400

    ObjectPack теперь по умолчанию тоже декларирует контекст как {}

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="objectpack",
-    version="0.7.3.8",
+    version="0.7.3.9",
     description=read('DESCRIPTION'),
     license="GPL",
     keywords="django m3 m3-contrib",
