901179739ab39ae5c005239507b43d0e19ba2cbe
Пирогов Алексей <devnull@localhost>
2013-10-10 14:39:33 +0400

    2.0.15

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="objectpack",
-    version="2.0.14",
+    version="2.0.15",
     license='Apache License, Version 2.0',
     description=read('DESCRIPTION'),
     author="Alexey Pirogov",
