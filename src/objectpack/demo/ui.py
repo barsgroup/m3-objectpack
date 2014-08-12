f8459a1671658ac8fe201c6a489d20022f94c318
Пирогов Алексей <devnull@localhost>
2014-08-12 11:24:48 +0400

    Колоночные фильтры теперь нормально работают и для вложенных полей (см демо)

--- src/objectpack/demo/ui.py
+++ src/objectpack/demo/ui.py
@@ -15,11 +15,11 @@ class GarageEditWindow(objectpack.TabbedEditWindow):
             model=models.Garage, field_list=('name',)
         ),
         objectpack.ObjectGridTab.fabricate_from_pack(
-            pack_name='objectpack.demo/StaffPack',
+            pack_name='objectpack.demo.actions.StaffPack',
             pack_register=controller.obs,
         ),
         objectpack.ObjectGridTab.fabricate_from_pack(
-            pack_name='objectpack.demo/ToolPack',
+            pack_name='objectpack.demo.actions.ToolPack',
             pack_register=controller.obs
         ),
     ]
