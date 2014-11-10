05d2c7aaf7814d913faf87b504cb3e12657632ec
Пирогов Алексей <devnull@localhost>
2014-11-10 16:48:08 +0300

    2.0.22.2 (setup.py)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="m3-objectpack",
-    version="2.0.22.1",
+    version="2.0.22.2",
     license='MIT',
     description=read('DESCRIPTION'),
     author="Alexey Pirogov",
