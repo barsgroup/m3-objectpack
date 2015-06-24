04275cee44c24363b527a07c68003fe3c829c971
Максим Елахов <elakhov@bars-open.ru>
2015-06-24 18:45:33 +0400

    Проверка наличия выбранного в окне множественного выбора из справочника

--- src/objectpack/templates/multi-select-window.js
+++ src/objectpack/templates/multi-select-window.js
@@ -41,10 +41,29 @@ Ext.apply(win, {
     }
 });
 
+function isGridSelected(grid, title, message){
+    res = true;
+    if (!grid.getSelectionModel().hasSelection() ) {
+        Ext.Msg.show({
+           title: title,
+           msg: message,
+           buttons: Ext.Msg.OK,
+           icon: Ext.MessageBox.INFO
+        });
+        res = false;
+    };
+    return res;
+}
+
 function selectValue() {
     var records = [], win, v;
     win = Ext.getCmp('{{ component.client_id }}');
     var grid = Ext.getCmp('{{ component.grid.client_id }}');
+
+    if (!isGridSelected(grid, 'Выбор элемента', 'Выберите элемент из списка') ) {
+        return;
+    }
+
     for (v in win.checkedItems) {
         if (win.checkedItems.hasOwnProperty(v) && win.checkedItems[v] !== undefined) {
             records.push(win.checkedItems[v]);
