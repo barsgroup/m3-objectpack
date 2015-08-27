c29ee7b229458ec2497446f1e89c85d90406e576
Максим Елахов <elakhov@bars-open.ru>
2015-08-27 16:28:54 +0400

    2.0.23.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="m3-objectpack",
-    version="2.0.23.2",
+    version="2.0.23.3",
     license='MIT',
     description=read('DESCRIPTION'),
     author="Alexey Pirogov",
