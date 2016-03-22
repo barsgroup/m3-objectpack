e935fa80301bba9403c5664fe4281b3207705431
Тимур Ахмадиев <akhmadiev@bars-open.ru>
2016-03-22 10:15:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="m3-objectpack",
-    version="2.0.23.4",
+    version="2.0.23.5",
     license='MIT',
     description=read('DESCRIPTION'),
     author="Alexey Pirogov",
