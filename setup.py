60a1518e0871dd27f9343909b4e6cab366685dbd
Pavel Gromov <pavel.gromov@fogstream.ru>
2016-07-11 21:41:30 +1000

    (EDUEPK-5197) Новая версия и ее описание в changelog'е

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="m3-objectpack",
-    version="2.0.23.5",
+    version="2.0.23.6",
     license='MIT',
     description=read('DESCRIPTION'),
     author="Alexey Pirogov",
