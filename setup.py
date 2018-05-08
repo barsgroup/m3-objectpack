cc67bc30c8a369e5919f768e32dcfc172f0c55cb
Андрей Чагочкин <andrey.chagochkin@bars-open.ru>
2018-05-08 12:02:49 +0700

    Поддержка pip 10.
    Т.к. в pip 10+ явно закрыто API, из кода удалено его использование.
    Ранее pip API использовался для чтения списка зависимостей из файла.
    Вместо этого зависимости перенесены в setup.py, а в файле со списком
    зависимостей указана точка, которая указывает pip брать зависимости из
    setup.py (для сохранения обратной совместимости).

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 # coding: utf-8
-import os
+from os.path import dirname
+from os.path import join
 
-from pip.download import PipSession
-from pip.req.req_file import parse_requirements
-from setuptools import setup, find_packages
-
-
-def _get_requirements(file_name):
-    pip_session = PipSession()
-    requirements = parse_requirements(file_name, session=pip_session)
-
-    return tuple(str(requirement.req) for requirement in requirements)
+from setuptools import find_packages
+from setuptools import setup
 
 
-def _read(fname):
-    try:
-        return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    except IOError:
-        return ''
+def _read(file_name):
+    with open(join(dirname(__file__), file_name)) as f:
+        return f.read()
+
 
 setup(
     name="m3-objectpack",
@@ -51,12 +43,20 @@ setup(
     packages=find_packages('src'),
     include_package_data=True,
     long_description=_read('README'),
-    install_requires=_get_requirements('REQUIREMENTS'),
     dependency_links=(
         'http://pypi.bars-open.ru/simple/m3-builder',
     ),
     setup_requires=(
-        'm3-builder>=1.1,<2',
+        'm3-builder>=1.2,<2',
     ),
-    set_build_info=os.path.dirname(__file__),
+    install_requires=(
+        'six>=1.11,<2',
+        'Django>=1.4,<2',
+        'm3-builder>=1.2,<2',
+        'm3-django-compat>=1.5.1,<2',
+        'django>=1.4,<2',
+        'm3-core>=2.2.16,<3',
+        'm3-ui>=2.2.40,<3',
+    ),
+    set_build_info=dirname(__file__),
 )
