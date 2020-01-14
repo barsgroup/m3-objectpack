6dba5fed0bdbcbe493af5463dcb6cb86c18311bd
sergey.kudrin <sergey.kudrin@bars-open.ru>
2020-01-14 14:21:02 +0700

    Исправлена ссылка на pypi.bars-open.ru для установки m3-builder

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     include_package_data=True,
     long_description=_read('README'),
     dependency_links=(
-        'http://pypi.bars-open.ru/simple/m3-builder',
+        'https://pypi.bars-open.ru/simple/m3-builder',
     ),
     setup_requires=(
         'm3-builder>=1.2,<2',
